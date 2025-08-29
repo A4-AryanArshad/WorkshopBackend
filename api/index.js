84892be300039fcbfee722f413d09291af0445da
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-08-29 17:28:36 +0500

    finance

--- api/index.js
+++ api/index.js
@@ -3001,16 +3001,16 @@ app.post('/api/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items_count: lineItems.length,
       mode: 'payment',
-      success_url: `${req.headers.origin}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${req.headers.origin}/payment-cancelled`
+      success_url: `https://workshopfrontend-one.vercel.app/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://workshopfrontend-one.vercel.app/payment-cancelled`
     });
     
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: `${req.headers.origin}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${req.headers.origin}/payment-cancelled`,
+      success_url: `https://workshopfrontend-one.vercel.app/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://workshopfrontend-one.vercel.app/payment-cancelled`,
       metadata: {
         customerEmail,
         customerName,
